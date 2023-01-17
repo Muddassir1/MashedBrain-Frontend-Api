1b98009e84d50fb264f7b759bc86e353919376aa
Muddassir Ahmed <muddassir.ah@gmail.com>
2023-01-17 17:24:54 +0500

    category images updated

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -21,15 +21,15 @@ class CategorySeeder extends Seeder
         $image_path = "/img/defaults/categories";
         DB::table('categories')->insert([
             ["name" => "Career & Success",
-            "image_path" => "$image_path/category-default.png"],
+            "image_path" => "$image_path/career-active.png"],
             ["name" => "Philosophy",
-            "image_path" => "$image_path/category-default.png"],
+            "image_path" => "$image_path/philosophy.png"],
             ["name" => "Politics",
-            "image_path" => "$image_path/category-default.png"],
+            "image_path" => "$image_path/politics.png"],
             ["name" => "History",
-            "image_path" => "$image_path/category-default.png"],
+            "image_path" => "$image_path/history.png"],
             ["name" => "Productivty",
-            "image_path" => "$image_path/category-default.png"]
+            "image_path" => "$image_path/productivity.png"]
         ]);
     }
 }
